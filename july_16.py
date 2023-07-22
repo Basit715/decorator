691a983ce845bf4666612ebcf9180ff12ec9609c
Basit715 <basitabass27411@gmail.com>
2023-07-22 19:05:45 +0530

    decorators

---
+++ july_16.py
@@ -0,0 +1,59 @@
+#function in python can be stored in data structures like list, hash table
+def greet(name):
+     return f"Welcome {name}"
+
+
+lst = []
+lst.append(greet)
+
+# print(lst[0]("Basit"))
+
+
+#function can be passed as an agrgument to another function
+
+def shout(text):
+     return text.upper()
+
+def whisper(text):
+     return text.lower()
+
+def greet(func, name):
+     greeting = func(f"Welcome {name}")
+     print(greeting)
+     
+# greet(shout, "Basit") 
+# greet(whisper, "Basit") 
+
+#function can be returned from another function
+def create_adder(x):
+     def adder(y):
+          return x + y
+     
+     return adder
+
+
+add_15 = create_adder(15)
+print(add_15(10))
+
+
+# ***************************decorators************************
+def hello_decorator(func):
+     def inner1():
+          print(f"Hello, this is before function execution")
+          
+          func()
+          
+          print(f"This is after function execution")
+          
+     return inner1
+
+def function_to_be_used():
+     print("This is inside the function!!!!")
+     
+     
+function_to_be_used = hello_decorator(function_to_be_used)
+
+function_to_be_used()
+
+
+
